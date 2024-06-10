3cea82395df3f2c8825cfaa3870dafb9bb412c88
VrajDarji <vrajdarji573@gmail.com>
2024-06-11 02:06:25 +0530

    initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from "@/providers/ThemeProvider";
+const inter = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning={true}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <body className={inter.className}>{children}</body>
+      </ThemeProvider>
     </html>
   );
 }
